5b07e51fe728f16ed856a017ba2c48c8e6e52efb
Javier Fernández Barreiro <javierfernandezbarreiro@gmail.com>
2020-07-08 07:47:53 +0200

    Init commit

---
+++ src/main/java/com/barrrettt/baseapi/controller/SaludoController.java
@@ -0,0 +1,20 @@
+package com.barrrettt.baseapi.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.barrrettt.baseapi.model.Saludo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    
+    @GetMapping("/saludo")
+	public Saludo greeting(@RequestParam(value = "nombre", defaultValue = "Mundo") String name) {
+		return new Saludo(counter.incrementAndGet(), String.format(template, name));
+	}
+}
